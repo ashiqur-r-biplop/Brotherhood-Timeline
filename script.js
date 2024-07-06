0b0f0fae054fd438a3588a70957439cb1abff52b
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-07 02:11:31 +0600

    added someocdE

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const timelineItems = document.querySelectorAll(".timeline-item");
+  const timeline = document.querySelector(".timeline");
 
   const isElementInViewport = (el) => {
-      const rect = el.getBoundingClientRect();
-      return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
   };
 
   const handleScroll = () => {
-      timelineItems.forEach((item) => {
-          if (isElementInViewport(item)) {
-              item.classList.add("in-view");
-          } else {
-              item.classList.remove("in-view");
-          }
-      });
+    if (isElementInViewport(timeline)) {
+    } else {
+      timeline.classList.remove("animate");
+    }
   };
 
-  window.addEventListener("scroll", handleScroll);
+  const throttle = (callback, limit) => {
+    let waiting = false;
+    return () => {
+      if (!waiting) {
+        callback();
+        waiting = true;
+        setTimeout(() => {
+          waiting = false;
+        }, limit);
+      }
+    };
+  };
+
+  window.addEventListener("scroll", throttle(handleScroll, 100));
   handleScroll(); // Check if any items are in view on initial load
 });
