44c7caf8011fb40d232449b0a63665ba7876f21e
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-07 01:43:51 +0600

    Added ospme cpode

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
   const timelineItems = document.querySelectorAll(".timeline-item");
 
   const isElementInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+      const rect = el.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
   };
 
   const handleScroll = () => {
-    timelineItems.forEach((item) => {
-      if (isElementInViewport(item)) {
-        item.classList.add("in-view");
-      }
-    });
+      timelineItems.forEach((item) => {
+          if (isElementInViewport(item)) {
+              item.classList.add("in-view");
+          } else {
+              item.classList.remove("in-view");
+          }
+      });
   };
 
   window.addEventListener("scroll", handleScroll);
